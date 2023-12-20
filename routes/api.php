725257fa0604b62270fe97d3316cb9c3631e8bdf
Mamonha <mamonha302@gmail.com>
2023-12-19 21:25:33 -0300

    Wip: api routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Produc;
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,12 +19,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
- Route::get('/create_products/', function (Request $request) {
-    $success = ProductsMongoDB::create([
-        'guid'=> 'cust_1111',
-        'sku' => '55',
-        'name'=> 'Vassoura',
-        'category' => 'ferramentas',
-        'description' => 'Teste apenas',
-    ]);
-}); 
+Route::prefix('products')->group(function () {
+    Route::post('/', [ProductController::class, 'create']); // Rota para criar um produto
+    Route::put('/{id}', [ProductController::class, 'update']); // Rota para atualizar um produto
+    Route::delete('/{id}', [ProductController::class, 'delete']); // Rota para excluir um produto
+});
