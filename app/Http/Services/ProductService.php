df6484b20e138e4206a1689b82bb39d37e9273b0
Mamonha <mamonha302@gmail.com>
2023-12-19 21:24:41 -0300

    Creating product service

---
+++ app/Http/Services/ProductService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Services;
+use App\Http\Repositories\ProductRepository;
+use App\Models\ProductsMongoDB;
+
+class ProductService
+{
+    protected $repository;
+
+    public function __construct(ProductRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function createProduct(array $data)
+    {
+        return $this->repository->create($data);
+    }
+
+    public function updateProduct($id, array $data)
+    {
+        $product = ProductsMongoDB::findOrFail($id);
+        return $this->repository->update($product, $data);
+    }
+
+    public function deleteProduct($id)
+    {
+        $product = ProductsMongoDB::findOrFail($id);
+
+        $this->repository->delete($product);
+    }
+}
