a65aba6992db438c6a8e2c37814ec82613734e69
Mamonha <mamonha302@gmail.com>
2023-12-19 22:17:14 -0300

    Define index and show method in Repository

--- app/Http/Repositories/ProductRepository.php
+++ app/Http/Repositories/ProductRepository.php
@@ -13,10 +13,19 @@ class ProductRepository
     public function update(ProductsMongoDB $product, array $data)
     {
         $product->update($data);
-
         return $product;
     }
 
+    public function show($id)
+    {
+        $product = ProductsMongoDB::findOrFail($id);
+        return $product;
+    }
+
+    public function index(){
+        return ProductsMongoDB::all();
+    }
+    
     public function delete(ProductsMongoDB $product)
     {
         $product->delete();
