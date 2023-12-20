861ab42c577d4d6a55d3677cafe28b58197738fa
Mamonha <mamonha302@gmail.com>
2023-12-19 21:24:11 -0300

    Creating product controller using service repository pattern in methods

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,18 +4,44 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\ProductsMongoDB;
+use App\Http\Services\ProductService;
+
 
 class ProductController extends Controller
 {
-    public function store(){  
-        $product = new ProductsMongoDB([
-            'guid'=> 'cust_1111',
-            'sku' => '55',
-            'name'=> 'Vassoura',
-            'category' => 'ferramentas',
-            'description' => 'Teste apenas',
+    protected $service;
+
+    public function __construct(ProductService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function create(Request $request)
+    {
+        $product = $this->service->createProduct($request->all());
+
+        return response()->json([
+            'message' => 'Product created successfully',
+            'product' => $product
         ]);
-       
-        return to_route('store.product', $product);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = $this->service->updateProduct($id, $request->all());
+
+        return response()->json([
+            'message' => 'Product updated successfully',
+            'product' => $product
+        ]);
+    }
+
+    public function delete($id)
+    {
+        $this->service->deleteProduct($id);
+
+        return response()->json([
+            'message' => 'Product deleted successfully'
+        ]);
     }
 }
